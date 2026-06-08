630e6afed53af6077ee96dfb5e285f6beee92232
alexandersimmnacher04-bit <alexandersimmnacher04@gmail.com>
2026-06-08 22:58:51 +0200

    Player Movement Teil 2

---
+++ Assets/Scenes/Alexander/Alexander_Assets/New Folder/Controller_ErstePerson.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Controller_ErstePerson : MonoBehaviour
+{
+    [Header("Movment Speeds")]
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
