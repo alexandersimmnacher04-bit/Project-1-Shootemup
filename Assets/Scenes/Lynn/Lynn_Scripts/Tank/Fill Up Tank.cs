afe4fa0e90f033c0c6599bb65a3dc0972bb2e2c0
llamaalynn <winkler.lynn1@gmail.com>
2026-07-06 12:50:57 +0200

    Tank puzzle
    Tank puzzle prototype done

--- Assets/Scenes/Lynn/Lynn_Scripts/Tank/Fill Up Tank.cs
+++ Assets/Scenes/Lynn/Lynn_Scripts/Tank/Fill Up Tank.cs
@@ -1,42 +1,86 @@
 using System;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
 
 //making puzzle where player fills up tank when holding mouse button down
 //maxtank capacity int = 100, current tank amount random between 0 and 100
 //when tank filled to correct amount, player can move on to next puzzle
+//if current tank amount is equal to max tank capacity, player can move on to next puzzle
+//make tank fill up over time, not instantly with OnMouseUp 
+//make UI slider to show current tank amount
+//player can overfill tank, cannot move onto next puzzle until tank is filled to correct amount
+
+
 
 public class FillUpTank : MonoBehaviour
 {
+    public float fillRate = 5f;
+    public float maxTank = 100f;
+    public float graceAmount = 2f;
+    float currentTank = 0f;
+    bool isFilling = false;
+    bool finished = false;
+    public Slider tankSlider;
 
-    int maxTank = 100;
-    int currentTank = 20;
-    bool isTankFull = false;
 
     void Start()
     {
-
+        currentTank = Random.Range(20, 70);
+        tankSlider.minValue = 0;
+        tankSlider.maxValue = maxTank + graceAmount;
+        tankSlider.value = currentTank;
+        Debug.Log("Current tank amount: " + currentTank);
     }
-
     void OnMouseDown()
     {
-        //fill up tank when mouse button is held down
-        //increase current tank amount by 1 every frame
-        //if current tank amount is equal to max tank capacity, stop filling up tank
-        //if current tank amount is equal to max tank capacity, player can move on to next puzzle
-        
-        while (currentTank < maxTank)
-        {
-            currentTank++;
-            Debug.Log(currentTank);
-        }
 
+            isFilling = true;
+            
 
-        isTankFull = true;
     }
 
+    void OnMouseUp()
+    {
+        //stop filling up tank when mouse button is released
+        //if current tank amount is equal to max tank capacity, player can move on to next puzzle
+        if (currentTank >= maxTank && currentTank <= maxTank + graceAmount)
+        {
+            Debug.Log("Tank is full! You can move on to the next puzzle.");
+             Win();
+        }
+        isFilling = false;
+    }
 
     void Update()
     {
+        if (finished) return;  
+     
+        if (isFilling)
+        {
+            currentTank += fillRate * Time.deltaTime;
+            tankSlider.value = currentTank;
+            Debug.Log(currentTank);
+            if (currentTank > maxTank + graceAmount) {
+                Lose();
+            }
+        }
+    }
 
+    void Win()
+    {
+        finished = true;
+        currentTank = maxTank;
+        tankSlider.value = currentTank;
+        Debug.Log("Yippie. Du hast es geschafft!");
+    }
+    void Lose()
+    {
+        finished = true;
+        isFilling = false;
+        Debug.Log("Du Loser oder whatever");
     }
 }
+
+
