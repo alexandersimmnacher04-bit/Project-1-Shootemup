fd7f1fda4f5b381266feb1881381fb64cd74fc14
alexandersimmnacher04-bit <alexandersimmnacher04@gmail.com>
2026-07-14 12:46:19 +0200

    Alexander Main Branch Merch

--- Assets/Scenes/Nolwen/animation/robot-arm/CameraManager.cs
+++ Assets/Scenes/Nolwen/animation/robot-arm/CameraManager.cs
@@ -3,13 +3,12 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Array mit allen Cameras
     [SerializeField] private Camera playerCamera;
     [SerializeField] private Camera robo_cam01;
     [SerializeField] private Camera robo_cam02;
 
     public bool IncamMode { get; private set; } = false;
-
+    // Hier wird gestzt auf welcher Kamera der Spieler gerde zugreift.
     public void SwitchToPlayer()
     {
         IncamMode = false;
@@ -32,5 +31,4 @@ public class CameraManager : MonoBehaviour
         robo_cam01.gameObject.SetActive(false);
         robo_cam02.gameObject.SetActive(true);
     }
-
 }
