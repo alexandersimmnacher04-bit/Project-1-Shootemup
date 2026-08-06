eba5ff8f1989894a67dba790c894a06514711ca3
alexandersimmnacher04-bit <alexandersimmnacher04@gmail.com>
2026-08-06 14:35:50 +0200

    roboterarm rätsel mit game manager verbunden

--- Assets/Scenes/Nolwen/animation/robot-arm/AblageZone.cs
+++ Assets/Scenes/Nolwen/animation/robot-arm/AblageZone.cs
@@ -4,11 +4,10 @@ public class AblageZone : MonoBehaviour
 {
     public Transform snapPunkt;
 
-    private void OnDrawGizmos()
-    {
-        Vector3 pos = (snapPunkt != null) ? snapPunkt.position : transform.position;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawWireCube(pos, new Vector3(0.3f, 0.3f, 0.3f));
-    }
+    public bool raeselSolved = false;
 
+    public void RegistriereAblage(GameManager abgelegtesObjekt)
+    {
+        raeselSolved = true;
+    }
 }
