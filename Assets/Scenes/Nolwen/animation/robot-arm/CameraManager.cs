2a1650e7d90eff77926855db4a4b100804172b34
Nolwen Kieran Nowotny <nonowotny@stud.mediadesign.de>
2026-08-10 22:29:29 +0200

    46. Commit | Update MainScene
    - adding camfilter effect to robocams

--- Assets/Scenes/Nolwen/animation/robot-arm/CameraManager.cs
+++ Assets/Scenes/Nolwen/animation/robot-arm/CameraManager.cs
@@ -6,6 +6,7 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private Camera playerCamera;
     [SerializeField] private Camera robo_cam01;
     [SerializeField] private Camera robo_cam02;
+    [SerializeField] private GameObject camfilter;
 
     public bool IncamMode { get; private set; } = false;
     // Hier wird gestzt auf welcher Kamera der Spieler gerde zugreift.
@@ -15,6 +16,8 @@ public class CameraManager : MonoBehaviour
         playerCamera.gameObject.SetActive(true);
         robo_cam01.gameObject.SetActive(false);
         robo_cam02.gameObject.SetActive(false);
+        camfilter.SetActive(false);
+        
     }
     public void SwitchTo01()
     {
@@ -22,6 +25,7 @@ public class CameraManager : MonoBehaviour
         playerCamera.gameObject.SetActive(false);
         robo_cam01.gameObject.SetActive(true);
         robo_cam02.gameObject.SetActive(false);
+        camfilter.SetActive(true);
     }
     public void SwitchTo02()
     {
@@ -29,5 +33,7 @@ public class CameraManager : MonoBehaviour
         playerCamera.gameObject.SetActive(false);
         robo_cam01.gameObject.SetActive(false);
         robo_cam02.gameObject.SetActive(true);
+        camfilter.SetActive(true);
+
     }
 }
